f70f0449f8bd112e8fbeffcb4469ed194abc950c
Rem Zolotykh <rem@zolotykh.net>
2017-03-24 22:02:42 +0800

    电影列表页面完成

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import StarRating from 'react-native-star-rating';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 import styleUtil from '../../utils/styleUtil';
@@ -6,6 +7,8 @@ import styleUtil from '../../utils/styleUtil';
 const { dw } = styleUtil;
 
 const movieWidth = (dw - 20 * 2 - 10) / 2 ;
+
+const imageHeight = (movieWidth * 297) / 200;
 
 const img = 'https://qnmob2.doubanio.com/view/movie_poster_cover/lpst/public/p480747492.jpg?imageView2/0/q/80/w/9999/h/200/format/jpg'
 
@@ -17,7 +20,17 @@ class Movie extends Component {
 				style={styles.movieImage}
 				source={{uri: img}}
 			/>
-			<Text>Movie</Text>
+			<Text style={styles.movieTitle}>Movie</Text>
+      <View style={styles.starRatingWrapper}>
+        <StarRating
+          disabled={true}
+          maxStars={5}
+          rating={3.5}
+          starSize={15}
+          starColor="#f5a623"
+        />
+        <Text style={styles.ratingText}>8.9</Text>
+      </View>
 		</View>
     );
   }
@@ -28,12 +41,25 @@ export default Movie;
 const styles = StyleSheet.create({
 	container: {
 		width: movieWidth,
-		height: 200,
-		backgroundColor: "yellow",
 		marginBottom: 10
 	},
 	movieImage: {
 		width:movieWidth,
-		height: 150
-	}
+		height: imageHeight
+  },
+  movieTitle: {
+    color: "#494949",
+    marginTop: 10,
+    marginBottom: 5,
+    textAlign: "center"
+  },
+  starRatingWrapper: {
+    height: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  ratingText: {
+    marginLeft: 5
+  }
 });
--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 import Movie from '../Movie/';
 
 class MovieList extends Component {
   render() {
     return (
+     <ScrollView showsVerticalScrollIndicator={true}>
       	<View style={styles.container}>
-			<Text>MovieList</Text>
-			<View style={styles.movieList}>
-				<Movie />
-				<Movie />
-				<Movie />
-				<Movie />
-				<Movie />
-				<Movie />
-			</View>
-		</View>
+          <View style={styles.movieList}>
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+          </View>
+	    </View>
+    </ScrollView>
     );
   }
 }
@@ -30,9 +31,9 @@ const styles = StyleSheet.create({
 	},
 	movieList: {
 		flex: 1,
+    alignItems: "center",
 		flexDirection: "row",
 		flexWrap: "wrap",
 		justifyContent: "space-between",
-		backgroundColor: "green"
 	}
 });
