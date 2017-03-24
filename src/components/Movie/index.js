b51b727cde873c54616b406d7009fa26e323e877
louisGan <louisGan1988@gmail.com>
2017-03-24 17:13:10 +0800

    增加图片

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 import styleUtil from '../../utils/styleUtil';
 
@@ -7,10 +7,16 @@ const { dw } = styleUtil;
 
 const movieWidth = (dw - 20 * 2 - 10) / 2 ;
 
+const img = 'https://qnmob2.doubanio.com/view/movie_poster_cover/lpst/public/p480747492.jpg?imageView2/0/q/80/w/9999/h/200/format/jpg'
+
 class Movie extends Component {
   render() {
     return (
       	<View style={styles.container}>
+			<Image
+				style={styles.movieImage}
+				source={{uri: img}}
+			/>
 			<Text>Movie</Text>
 		</View>
     );
@@ -25,5 +31,9 @@ const styles = StyleSheet.create({
 		height: 200,
 		backgroundColor: "yellow",
 		marginBottom: 10
+	},
+	movieImage: {
+		width:movieWidth,
+		height: 150
 	}
 });
